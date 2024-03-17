e08ce156ced3b4fa9d2e796bf5318453b9cb0f12
unknown <rai2889@gmail.com>
2024-03-17 22:13:01 +0530

    cors

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const connection = require("./config/db")
 const userRouter = require("./routes/user.route")
 const gameRouter = require("./routes/game.route")
 const cardRouter = require("./routes/card.route")
+const cors =require("cors")
 const app = express()
 
 app.use(express.json())
-
+app.use(cors())
 app.use("/user",userRouter)
 app.use("/game", gameRouter)
 app.use("/card", cardRouter)
